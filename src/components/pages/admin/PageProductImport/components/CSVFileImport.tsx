109103a26302258a2be580d2a7ac81ff33c652ce
Pavel Dobrenko <dobrenko92@gmail.com>
2020-11-29 19:52:10 +0300

    feat: task-5 add notifications

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -31,6 +31,8 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
     };
 
     const uploadFile = async (e: any) => {
+        const auth = localStorage.getItem('authorization_token');
+
         // Get the presigned URL
         const response = await axios({
             method: 'GET',
@@ -38,6 +40,7 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
             params: {
                 name: encodeURIComponent(file.name),
             },
+            headers: { ...(auth && { Authorization: `Basic ${btoa(auth)}` }) },
         });
         console.log('File to upload: ', file.name);
         console.log('Uploading to: ', response.data);
