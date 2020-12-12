409231fe0faea0708aa85404cc104437df9ffd8f
kotmatpockuh <dobrenko92@gmail.com>
2020-12-12 19:10:52 +0300

    Task-7 (#6)
    * feat: reformat code
    
    * feat: task-5 add notifications

--- src/index.tsx
+++ src/index.tsx
@@ -2,32 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'index.css';
 import App from 'components/App/App';
-import {store} from 'store/store';
-import {Provider} from 'react-redux';
+import { store } from 'store/store';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import CssBaseline from "@material-ui/core/CssBaseline";
+import CssBaseline from '@material-ui/core/CssBaseline';
 import axios from 'axios';
 
 axios.interceptors.response.use(
-  response => {
-    return response;
-  },
-  function(error) {
-    if (error.response.status === 400) {
-      alert(error.response.data?.data);
+    (response) => {
+        return response;
+    },
+    function (error) {
+        if (error.response.status === 400) {
+            alert(error.response.data?.data);
+        } else if (error.response.status === 401) {
+            alert('⚠️ Corrupted Authorization');
+        } else if (error.response.status === 403) {
+            alert('🔐 Provided user credentials are invalid');
+        } else {
+            alert('😢 Something went wrong');
+        }
+        return Promise.reject(error.response);
     }
-    return Promise.reject(error.response);
-  }
 );
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <CssBaseline/>
-      <App/>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <CssBaseline />
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
