4339c1f3d392b5d282924210d27bd03f33ecf29a
Pavel Dobrenko <dobrenko92@gmail.com>
2020-12-12 23:41:55 +0300

    feat: update cart url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://2uvvqem7lk.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://kotmatpockuh-cart-api-aws-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
